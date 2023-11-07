5f56c3d8e2cc8eabaa311f7f67318108c0ab6a4d
ryland <ryland@MacBook-Pro-4.hawaii.rr.com>
2023-11-07 02:09:19 -0800

    finished views and some custom views

--- server/settings.py
+++ server/settings.py
@@ -28,10 +28,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.environ['MODE'] == 'dev' else False
+DEBUG = 'RENDER' not in os.environ
+# DEBUG = True if os.environ['MODE'] == 'dev' else False
 
 ALLOWED_HOSTS = []
 
+## Handling Allowed Hosts on Render
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
--- surflog/views.py
+++ surflog/views.py
@@ -7,15 +7,18 @@ from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from .helpers import GetBody
+from users.models import User
 # Create your views here.
 
 class SpotList(generics.ListCreateAPIView):
     # authentication_classes = [TokenAuthentication]
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     queryset = Spot.objects.all()
     serializer_class = SpotSerializer
 
 class SpotDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Spot.objects.all()
     serializer_class = SpotSerializer
 
@@ -29,8 +32,20 @@ class SurfsessionList(generics.ListCreateAPIView):
         serializer = SurfsessionSerializer(queryset, many=True)
         # return Response(request.user.id)
         return Response(serializer.data)
+    def post(self, request):
+        body = GetBody(request)
+        print(body)
+        print(request.user.id)
+        spot_id = body["spot"]
+        user_id = request.user.id  
+        spot = Spot.objects.get(pk=spot_id)
+        user = User.objects.get(pk=user_id)
+        queryset = Surfsession.objects.create(notes=body["notes"], board=body["board"], size=body["size"], spot=spot, user=user)
+        serializer = SurfsessionSerializer(queryset)
+        return Response(serializer.data)
 
 class SurfsessionDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Surfsession.objects.all()
     serializer_class = SurfsessionSerializer
 
